0c0706c0af46f7ebbc841d73a770c9955ea155aa
Guilherme de Paula <guilhermedepaula.ti@gmail.com>
2024-01-18 23:02:30 -0300

    feat: tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
